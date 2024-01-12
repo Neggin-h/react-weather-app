33974a67a473f23de1bd797325dcbf697cfc56f9
Neggin-h <negginhariri@gmail.com>
2024-01-12 20:31:34 +0330

    Made the initial app without CSS

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by Neggin Hariri and is{" "}
+          <a
+            href="https://github.com/Neggin-h/react-weather-app.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Weather from "./weather";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <p>
-        Coded by
-        <a href="https://github.com/Neggin-h/react-weather-app.git">
-          Neggin Hariri
-        </a>
-      </p>
-    </div>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
